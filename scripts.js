db3368e7ac5100a20af1a5be14def99fe0a0de8c
anthgrim <anthgrim@icloud.com>
2022-01-30 16:40:57 -0500

    Added more phrases in switch case and created separate class for button hover

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ const delayedColorChange = (color, delay) => {
 const phraseButton = document.getElementById("phraseButton");
 
 const randomNumber = () => {
-    const number = Math.floor(Math.random() * 10);
+    const number = Math.floor(Math.random() * 20);
     return number;
 }
 
@@ -46,6 +46,36 @@ phraseButton.addEventListener('click',()=>{
             break;
         case 9:
             alert("“Listen, I’m not the nicest guy in the universe, because I’m the smartest, and being nice is something stupid people do to hedge their bets.” — Rick");
+            break;
+        case 10:
+            alert("“I know that new situations can be intimidating. You’re lookin’ around and it’s all scary and different, but y’know, meeting them head-on, charging into ‘em like a bull — that’s how we grow as people.” — Rick");
+            break;
+        case 11:
+            alert("“You gotta do it for Grandpa, Morty. You gotta put these seeds inside your butt.” — Rick");
+            break;
+        case 12:
+            alert("“Honey, stop raising your father’s cholesterol so you can take a hot funeral selfie.” — Beth");
+            break;
+        case 13:
+            alert("“So what if he’s the devil, Rick? At least the devil has a job. At least he’s active in the community.” — Summer");
+            break;
+        case 14:
+            alert("“Unity, I’m sorry. I didn’t know freedom meant people doing stuff that sucks. I was thinking more of a ‘choose your own cellphone carrier’ thing.” — Summer");
+            break;
+        case 15:
+            alert("“Excuse me. Coming through. What are you here for? Just kidding, I don’t care.” — Rick");
+            break;
+        case 16:
+            alert("“Yeah, sure, I mean, if you spend all day shuffling words around, you can make anything sound bad, Morty.” — Rick");
+            break;
+        case 17:
+            alert("“Don’t get drawn into the culture, Morty. Stealing stuff is about the stuff, not the stealing.” — Rick");
+            break;
+        case 18:
+            alert("“I’m sorry, but your opinion means very little to me.” — Rick");
+            break;
+        case 19:
+            alert("“B*tch, my generation gets traumatized for breakfast!” — Summer");
             break;
         default:
             alert("“I’m a scientist; because I invent, transform, create, and destroy for a living, and when I don’t like something about the world, I change it.” — Pickle Rick");
